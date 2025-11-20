5c32f396c3143e74e84f03c7c7519cb86caa5ed9
Baptiste <bpt.cor@gmail.com>
2025-11-20 15:18:06 +0100

    add: fonction virement entre les comptes

--- src/main/java/com/example/proxibanque/service/CompteService.java
+++ src/main/java/com/example/proxibanque/service/CompteService.java
@@ -1,4 +1,37 @@
 package com.example.proxibanque.service;
 
+import com.example.proxibanque.model.Compte;
+import com.example.proxibanque.repository.CompteRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
 public class CompteService {
+
+    private final CompteRepository compteRepository;
+
+    @Transactional
+    public void Virement(String numeroCompteDebit, String numeroCompteCredit, double montant) {
+
+        Compte compteDebit = compteRepository.findByNumeroCompte(numeroCompteDebit);
+        Compte compteCredit = compteRepository.findByNumeroCompte(numeroCompteCredit);
+
+        if (compteDebit == null || compteCredit == null)
+            throw new NoSuchElementException("Erreur: Un des comptes n'existe pas.");
+
+        if (montant <= 0)
+            throw new IllegalArgumentException("Erreur: Le montant doit être positif.");
+
+        // A voir pour apres : check le decouvert
+        compteDebit.setSolde(compteDebit.getSolde() - montant);
+
+        compteCredit.setSolde(compteCredit.getSolde() + montant);
+
+        compteRepository.save(compteDebit);
+        compteRepository.save(compteCredit);
+    }
 }
