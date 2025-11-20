e5168547af8c2875bb539019ea0329abf0b55537
Baptiste <bpt.cor@gmail.com>
2025-11-20 16:38:51 +0100

    suprr: old idea projet

--- src/main/java/com/example/proxibanque/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proxibanque.repository;
-
-import com.example.proxibanque.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- src/main/java/com/example/proxibanque/repository/CompteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.proxibanque.repository;
-
-import com.example.proxibanque.model.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-    // normalement ca marche
-    Compte findByNumeroCompte(String numeroCompte);
-}
